047c4156863decca51a501592bbdd3cb6ad76734
Jurij-Gagarin <schoenfeld.jurij@gmail.com>
2022-02-18 13:45:22 +0100

    Regional multiplots now working

--- plot.py
+++ plot.py
@@ -8,6 +8,7 @@ import numpy as np
 import calendar
 from dateutil.rrule import rrule, MONTHLY
 from scipy.interpolate import CubicSpline
+import helpful_functions as hf
 
 
 class VarOptions:
@@ -37,6 +38,65 @@ class VarOptions:
         return f'{self.name} in {self.unit}' + extra_label
 
 
+class RegionalPlot:
+    # Note that n * 10 can bee plotted perfectly fine
+    def __init__(self, date1, date2, variable, extent=ci.arctic_extent, fig_shape=(2, 5), show=False):
+        self.fig_shape = fig_shape
+        self.extent = extent
+        self.dates = ds.time_delta(date1, date2)
+        self.show = show
+        self.variable = variable
+        self.plot_leads = False
+        for var in self.variable:
+            if var == 'leads':
+                self.variable.remove('leads')
+                self.plot_leads = True
+
+        # first we need to set up the plot
+        self.full = int(np.ceil(len(self.dates) / (fig_shape[0] * fig_shape[1])))  # number of figures necessary
+
+        for i in range(self.full):
+            fig, ax = self.setup_plot(i)
+
+            for j, a in enumerate(ax.flatten()):
+                date = self.dates[j + self.fig_shape[0] * self.fig_shape[1] * i]
+                print(date)
+                self.regional_var_plot(fig, a, date)
+
+            show_plot(fig, f'./test{i}.png', self.show)
+
+    def setup_plot(self, base):
+        # create figure and base map
+        fig, ax = plt.subplots(self.fig_shape[0], self.fig_shape[1], figsize=(20, 15),
+                               subplot_kw={"projection": ccrs.NorthPolarStereo(-45)})
+        # fig.set_size_inches(32, 18)
+        print(ax.flatten())
+        for i, a in enumerate(ax.flatten()):
+            a.set_title(ds.string_time_to_datetime(self.dates[i + self.fig_shape[0] * self.fig_shape[1] * base]))
+            a.gridlines()
+            a.set_global()
+            a.coastlines(resolution='50m')
+            a.set_extent(self.extent, crs=ccrs.PlateCarree())
+        return fig, ax
+
+    def regional_var_plot(self, fig, ax, date):
+        # setup data
+        lead = leads.Lead(date)
+        grid = leads.CoordinateGrid()
+
+        # plot lead data
+        if self.plot_leads:
+            lead_plot(grid, lead, fig, ax, False)
+
+        # plot variable data
+        if self.variable:
+            if isinstance(self.variable, list):
+                for v in self.variable:
+                    variable_plot(date, fig, ax, v, False)
+            else:
+                variable_plot(date, fig, ax, self.variable, False)
+
+
 def setup_plot(extent):
     # create figure and base map
     fig, ax = plt.subplots()
@@ -59,7 +119,7 @@ def show_plot(fig, file_name, show):
         plt.close(fig)
 
 
-def regional_var_plot(date, extent=None, show=False, variable=None, plot_leads=True):
+def regional_var_plot(date, extent=None, show=False, variable=None, plot_leads=True, show_cbar=False):
     # Creates a regional plot of your data.
     # Supported data: Leads, wind, cyclone_occurence, msl and t2m.
     file_name = date
@@ -74,17 +134,17 @@ def regional_var_plot(date, extent=None, show=False, variable=None, plot_leads=T
 
     # plot lead data with color-bar
     if plot_leads:
-        lead_plot(grid, lead, fig, ax)
+        lead_plot(grid, lead, fig, ax, show_cbar)
 
     # plot variable data with color-bar
     if variable:
         if isinstance(variable, list):
             for v in variable:
-                variable_plot(date, fig, ax, v)
+                variable_plot(date, fig, ax, v, show_cbar)
                 file_name = v + '_' + file_name
                 title += ', ' + VarOptions(v).label()
         else:
-            variable_plot(date, fig, ax, variable)
+            variable_plot(date, fig, ax, variable, show_cbar)
             file_name = variable + '_' + file_name
             title += ', ' + VarOptions(variable).label()
 
@@ -128,17 +188,18 @@ def two_lead_diff_plot(date1, date2, extent=None, file_name=None, show=False, ms
     show_plot(fig, file_name, show)
 
 
-def lead_plot(grid, lead, fig, ax):
+def lead_plot(grid, lead, fig, ax, show_cbar):
     # Plots lead fraction
     im = ax.pcolormesh(grid.lon, grid.lat, 100 * lead.lead_data, cmap='cool', transform=ccrs.PlateCarree())
     ax.pcolormesh(grid.lon, grid.lat, lead.water, cmap='coolwarm', transform=ccrs.PlateCarree())
     ax.pcolormesh(grid.lon, grid.lat, lead.land, cmap='twilight', transform=ccrs.PlateCarree())
     ax.pcolormesh(grid.lon, grid.lat, lead.cloud, cmap='Blues', transform=ccrs.PlateCarree())
-    cbar = fig.colorbar(im, ax=ax)
-    cbar.ax.tick_params(labelsize=17)
+    if show_cbar:
+        cbar = fig.colorbar(im, ax=ax)
+        cbar.ax.tick_params(labelsize=17)
 
 
-def variable_plot(date, fig, ax, variable):
+def variable_plot(date, fig, ax, variable, show_cbar):
     # Plots data that is stored in a Era5 grid.
     Var = VarOptions(variable)
     data_set = leads.Era5(variable)
@@ -152,8 +213,9 @@ def variable_plot(date, fig, ax, variable):
         im = ax.pcolormesh(data_set.lon, data_set.lat, data_set.get_variable(date), cmap=Var.cmap,
                            alpha=Var.alpha, transform=ccrs.PlateCarree())
         # im.set_clim(0, 25)
-        cbar = fig.colorbar(im, ax=ax)
-        cbar.ax.tick_params(labelsize=17)
+        if show_cbar:
+            cbar = fig.colorbar(im, ax=ax)
+            cbar.ax.tick_params(labelsize=17)
 
 
 def matrix_plot(date1, date2, variable, clim=(None, None), extent=None, show=False):
@@ -194,11 +256,12 @@ def variables_against_time(date1, date2, extent, var1, var2, spline=False, show=
     fig, ax = plt.subplots(figsize=(20, 10))
     ax_twin = ax.twinx()
     Var1, Var2 = VarOptions(var1), VarOptions(var2)
-    title = f'Changes in {Var1.name} and {Var2.name} over time within {extent}.'
+    title = f'Changes in {Var1.name} and {Var2.name} in the {ci.extent_dict[extent]}.'
     y, i = [], 0
 
     for a, v, Var in zip([ax, ax_twin], [var1, var2], [Var1, Var2]):
         y.append(ds.variable_daily_avg(date1, date2, extent, v))
+        y[i] = y[i] / np.max(y[i])
 
         if spline:
             x = list(range(len(dates)))
@@ -210,29 +273,31 @@ def variables_against_time(date1, date2, extent, var1, var2, spline=False, show=
         else:
             a.plot(dates, y[i], c=Var.color, linestyle='--')
 
-        a.set_ylabel(Var.label(), fontsize=15)
+        a.set_ylabel(f'{Var.name} normalized', fontsize=15)
         a.yaxis.label.set_color(Var.color)
         a.tick_params(axis='y', colors=Var.color, labelsize=15)
         i += 1
 
     ax.tick_params(axis='x', labelsize=15)
-    title += f' R = {np.corrcoef(y[0], y[1])[0, 1]}'
+    title += f' R = {hf.round_sig(np.corrcoef(y[0], y[1])[0, 1])}'
     ax.set_title(title, fontsize=15)
 
     show_plot(fig, f'./plots/{var1}_{var2}_{ds.string_time_to_datetime(date1)}_'
-                   f'{ds.string_time_to_datetime(date2)}_{extent}.png', show)
+                   f'{ds.string_time_to_datetime(date2)}_{ci.extent_dict[extent]}.png', show)
 
 
 def plot_lead_from_vars(date1, date2, extent, var1, var2):
     par, cov, fit, v1, v2, l = ds.lead_from_vars(date1, date2, extent, var1, var2)
     dates = ds.string_time_to_datetime(ds.time_delta(date1, date2))
 
-    plt.plot(dates, l)
+    plt.plot(dates, l, label='leads')
     fit_data = [fit(x, par[0], par[1]) for x in zip(v1, v2)]
-    plt.plot(dates, fit_data)
-    plt.plot(dates, v1)
-    plt.plot(dates, v2)
+    plt.plot(dates, fit_data, label='a*cyc+b*sic')
+    plt.plot(dates, v1, label=var1)
+    plt.plot(dates, v2, label=var2)
+
     print(par)
+    plt.legend()
     plt.show()
 
 
@@ -276,12 +341,19 @@ def plots_for_case(case, extent=None, var=None, plot_lead=True, diff=False):
 
 
 if __name__ == '__main__':
-    # regional_var_plot('20200219', show=True, variable=['cyclone_occurence', 'msl'], plot_leads=True)
-    # variable_avg_sum_daily('20200101', '20200331', no_extent, ('msl', 'cyclone_occurence'))
-    # variables_against_time('20200101', '20200331', ci.arctic_extent, 'leads', 'cyclone_occurence')
-    # variables_against_time('20200101', '20200115', ci.arctic_extent, 'leads', 'siconc')
-    # variables_against_time('20200101', '20200331', s_extent, 'leads', 'cyclone_occurence')
-    #matrix_plot('20200320', '20200325', 'leads', extent=ci.s_extent, show=True)
+    # regional_var_plot('20200219', show=True, variable=['cyclone_occurence', 'msl'], plot_leads=True,
+                      # extent=ci.extent1, show_cbar=False)
+
+    RegionalPlot('20200215', '20200225', ['leads', 'msl', 'cyclone_occurence'], extent=ci.barent_extent, show=True)
+
+    # matrix_plot('20200320', '20200325', 'leads', extent=ci.s_extent, show=True)
     # plot_lead_cyclone_sum_monthly('20191101', '20200430', no_extent, 'cyclone_occurence')
-    plot_lead_from_vars('20200101', '20200131', ci.arctic_extent, 'cyclone_occurence', 'wind')
+    # plot_lead_from_vars('20200101', '20200131', ci.arctic_extent, 'cyclone_occurence', 'siconc')
+
+    '''
+    for d in ci.Mon:
+        variables_against_time(d[0], d[1], ci.arctic_extent, 'leads', 'siconc')
     pass
+    '''
+    # variables_against_time('20200120', '20200210', ci.arctic_extent, 'leads', 'cyclone_occurence')
+    # variables_against_time('20200120', '20200210', ci.arctic_extent, 'leads', 'siconc')
